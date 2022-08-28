7246a2977480a21b6a5b71d0930b725bc48e2261
Ali <36738510+MrAliSalehi@users.noreply.github.com>
2022-08-29 04:28:15 +0430

    more cert shits

--- Common/Extensions/CertificateExtensions.cs
+++ Common/Extensions/CertificateExtensions.cs
@@ -6,14 +6,14 @@ internal static class CertificateExtensions
 {
     public static X509Certificate2 GetCertificate(this string thumbPrint)
     {
-        using var store = new X509Store(StoreName.CertificateAuthority, StoreLocation.LocalMachine);
+        using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
 
         store.Open(OpenFlags.ReadOnly);
         var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbPrint, false);
 
         if (certCollection.Count is 0)
         {
-            throw new ArgumentException($"Certificate Is Not Installed-Store location:[{store.Location}]");
+            throw new ArgumentException($"Certificate Is Not Installed-Store location:[{store.Location}]\nall Cert Count:{store.Certificates.Count}");
         }
         return certCollection.First();
     }
